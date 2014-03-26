47288eb2e307544a2a2b4b621994c232f431768a
lanou3g <lanou3g@lanou3gdeMac-mini-8.local>
2014-03-26 13:34:36 +0800

    Initial Commit

---
+++ FacePlusPlusDemo_and_SDK/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacePlusPlusDemo_and_SDK
+//
+//  Created by lanou3g on 14-3-26.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FacePlusPlusDemo_and_SDK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FacePlusPlusDemo_and_SDK
+//
+//  Created by lanou3g on 14-3-26.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
